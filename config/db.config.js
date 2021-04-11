64b5afcd7aa8b56776d6641351c7cf78195d6cf9
Shashank Srivastava <shashank.srivastava@tothenew.com>
2021-04-11 15:36:22 +0530

    Table create query

--- config/db.config.js
+++ config/db.config.js
@@ -6,7 +6,7 @@ const dbConn = mysql.createConnection('mysql://be479c845b62e4:a4da7bcd@us-cdbr-e
 dbConn.connect(function(err) {
   if (err) throw err;
   console.log("Database Connected!");
-  var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(255), email VARCHAR(100), phone VARCHAR(255))";
+  var sql = "CREATE TABLE employees (id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(255), email VARCHAR(100), phone VARCHAR(255))";
   dbConn.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
